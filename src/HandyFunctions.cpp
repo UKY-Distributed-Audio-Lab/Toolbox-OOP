90dff52bd643916501e19e2138d13599e0123125
Grant Cox <grantcox167@gmail.com>
2019-01-01 16:38:33 -0500

    working, need to copy positive index coefficients to negative side

--- src/HandyFunctions.cpp
+++ src/HandyFunctions.cpp
@@ -34,27 +34,21 @@ mat resize_and_zero_pad(std::vector<Col<double>> & newData) {
 //frequency sampling fir design
 //https://www.allaboutcircuits.com/technical-articles/design-of-fir-filters-using-frequency-sampling-method/
 vec fir2(int n_order, vec freq, vec mag) { 
-    printf("freq:\n"); freq.print(); 
-    printf("mag:\n"); mag.print();
     int M = (n_order - 1) / 2;
+    
     vec pos_coeffs(M+1); //coefficiencts from 0 to M
-    vec pos_freq(M+1), pos_mag(M+1); //subvector from 0 to max positive index
+    vec pos_freq = freq.subvec(M, n_order-1);
+    vec pos_mag  = mag.subvec(M, n_order-1); //subvector from 0 to max positive index
     vec h; //to return
     
-    for(uint16_t i = 0; i < M; i++) { //copy to subvectors
-        printf("%d\r",i);
-        pos_freq(i) = freq(i+M+1);
-        pos_mag(i) = mag(i+M+1);
-    }
-
-    for(uint16_t n = 0; n < M+1; n++) {
+    for(uint16_t n = 0; n <= M; n++) {
         printf("\n\n--------------------n=%d------------------\n",n);
-        int tmp_sum = 0;
-        for(uint16_t k = 0; k < M+1; k++)   {
-            printf("%d += %f * cos(%d * %f)\tk=%d\n", tmp_sum, pos_mag(k), n, pos_freq(k), k);
+        float tmp_sum = 0;
+        for(uint16_t k = 1; k <= M; k++)   {
+            printf("%f += %f * cos(%d * %f)\tk=%d\n", tmp_sum, pos_mag(k), n, pos_freq(k), k);
             tmp_sum += pos_mag(k) * cos(n * pos_freq(k));
         }
-        printf("pos_coeffs(%d) = (1/(2*%d+1)) * (%d + 2*%f)\n\n", n, M, pos_mag(0), tmp_sum);
+        printf("pos_coeffs(%d) = (1/(2*%d+1)) * (%f + 2*%f)\n\n", n, M, pos_mag(0), tmp_sum);
         pos_coeffs(n) = (1/(2*(float)M+1)) * (pos_mag(0) + 2*tmp_sum);
     }
     printf("\npos_coeffs:\n"); pos_coeffs.print();
