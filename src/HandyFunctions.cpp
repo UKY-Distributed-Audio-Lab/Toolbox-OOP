610c11c801259429190c5d940983e6c0c4b4cfac
Grant Cox <grantcox167@gmail.com>
2019-01-01 23:44:42 -0500

    finished copying to negative indeces in the array. Implemented input checking. Wrote more vigorous test cases.

--- src/HandyFunctions.cpp
+++ src/HandyFunctions.cpp
@@ -34,24 +34,43 @@ mat resize_and_zero_pad(std::vector<Col<double>> & newData) {
 //frequency sampling fir design
 //https://www.allaboutcircuits.com/technical-articles/design-of-fir-filters-using-frequency-sampling-method/
 vec fir2(int n_order, vec freq, vec mag) { 
+    if(!(n_order % 2)) {
+        fprintf(stderr,"ERROR in fir2: must request an odd-order FIR filter\n");
+        exit(1);
+    }
+    if(freq.size() != n_order) {
+        fprintf(stderr,"ERROR in fir2: size of vec freq does not equal the order requested\n");
+        exit(1);
+    }
+    if(mag.size() != n_order) {
+        fprintf(stderr,"ERROR in fir2: size of vec mag does not equal the order requested\n");
+        exit(1);
+    }
+
+
     int M = (n_order - 1) / 2;
     
     vec pos_coeffs(M+1); //coefficiencts from 0 to M
     vec pos_freq = freq.subvec(M, n_order-1);
     vec pos_mag  = mag.subvec(M, n_order-1); //subvector from 0 to max positive index
-    vec h; //to return
+    vec h = zeros(n_order); //to return
     
     for(uint16_t n = 0; n <= M; n++) {
-        printf("\n\n--------------------n=%d------------------\n",n);
         float tmp_sum = 0;
         for(uint16_t k = 1; k <= M; k++)   {
-            printf("%f += %f * cos(%d * %f)\tk=%d\n", tmp_sum, pos_mag(k), n, pos_freq(k), k);
             tmp_sum += pos_mag(k) * cos(n * pos_freq(k));
         }
-        printf("pos_coeffs(%d) = (1/(2*%d+1)) * (%f + 2*%f)\n\n", n, M, pos_mag(0), tmp_sum);
         pos_coeffs(n) = (1/(2*(float)M+1)) * (pos_mag(0) + 2*tmp_sum);
     }
-    printf("\npos_coeffs:\n"); pos_coeffs.print();
+    
+    //copy results to return variable h
+    h(M) = pos_coeffs(0);
+    uint16_t neg_index_offset = 2;
+    for(uint16_t i = M+1; i < n_order; i++) {
+        h(i) = pos_coeffs(i - M);
+        h(i - neg_index_offset) = h(i);
+        neg_index_offset += 2;
+    }
     return h;
 }
 
--- src/main.cpp
+++ src/main.cpp
@@ -3,12 +3,22 @@
 
 
 int main(int argc, char* argv[]) {
-    vec freq = {-8*M_PI/9, -6*M_PI/9, -4*M_PI/9, -2*M_PI/9, 0, 2*M_PI/9, 4*M_PI/9, 6*M_PI/9, 8*M_PI/9}, 
-        mag = {0, 0, 0, 1, 1, 1, 0, 0, 0}, 
-        h;
-    uint8_t order = 9;
+    vec freq(25);
+    freq(12) = 0; //0 point of frequency
+    uint8_t neg_side_offset = 2;
+    for(int i = 13; i < 25; i++) { //create 25 order fir 
+        //12 on each side of 0
+        freq(i) = neg_side_offset * M_PI / 25;
+        freq(i - neg_side_offset) = -freq(i);
+        neg_side_offset += 2;
+    }
+    printf("freq:\n");  freq.print();
 
-    h = fir2(order, freq, mag);
+    vec mag = {0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+    printf("mag:\n"); mag.print();
+    uint8_t order = 25;
+
+    vec h = fir2(order, freq, mag);
     printf("\nh:\n");
     h.print();
 }
